358a6b973fd09aaf703b201f71b73b30744e3bd6
kamanisrivarsha <121373704+kamanisrivarsha@users.noreply.github.com>
2023-01-12 20:00:02 +0530

    Add files via upload

---
+++ exe.java
@@ -0,0 +1,10 @@
+public class exe
+{
+public static void main(String[] args)
+{
+String str="Varsha";
+System.out.println("String str" +str);
+int index=str.charAt(5);
+System.out.println("The Character at position 5 is" +(char)index);
+}
+}
